1c35c640c1b72d995b9ceb47c80755ec360f6117
Rohan Pillai <32511124+rohanpillai20@users.noreply.github.com>
2018-07-05 14:42:21 +0530

    Added Comments and Output snippets

--- Merge-Cells-Python.py
+++ Merge-Cells-Python.py
@@ -1,8 +1,10 @@
+# Import Files
 import pandas as pd
 from openpyxl.workbook import Workbook
 from openpyxl.utils.dataframe import dataframe_to_rows 
 exportPath = r'C:\Users\Rohan\Desktop\PythonExport.xlsx'
 
+# Creating a dictionary
 dict1={}
 
 dict1['column1']=['r_n_1','r_n_1','r_n_1','r_n_1','r_n_10', 'r_n_10']
@@ -11,13 +13,24 @@ dict1['column3']=['r_n_2','r_n_4','r_n_6','r_n_6','r_n_11', 'r_n_11']
 dict1['column4']=['r_s_3','r_s_4','r_s_5','r_s_6','r_s_8', 'r_s_10']
 dict1['column5']=['r_n_3','r_n_5','r_n_7','r_n_9','r_n_12', 'r_n_13']
 
-wb= Workbook()
-ws=wb.active
-
+#Converting the dictionary to a Data Frame
 df = pd.DataFrame(dict1)
 
 print(df)
 
+"""
+Output:
+
+  column1 column2 column3 column4 column5
+0   r_n_1   r_s_1   r_n_2   r_s_3   r_n_3
+1   r_n_1   r_s_1   r_n_4   r_s_4   r_n_5
+2   r_n_1   r_s_1   r_n_6   r_s_5   r_n_7
+3   r_n_1   r_s_1   r_n_6   r_s_6   r_n_9
+4  r_n_10   r_s_7  r_n_11   r_s_8  r_n_12
+5  r_n_10   r_s_9  r_n_11  r_s_10  r_n_13
+"""
+
+# To identify the repeating values
 for i in df.columns:
     for j in range(len(df[i])):
         indf=0
@@ -26,6 +39,22 @@ for i in df.columns:
                 df[i][k]='-' 
 print("")
 print(df)
+
+"""
+Output:
+
+  column1 column2 column3 column4 column5
+0   r_n_1   r_s_1   r_n_2   r_s_3   r_n_3
+1       -       -   r_n_4   r_s_4   r_n_5
+2       -       -   r_n_6   r_s_5   r_n_7
+3       -       -       -   r_s_6   r_n_9
+4  r_n_10   r_s_7  r_n_11   r_s_8  r_n_12
+5       -   r_s_9       -  r_s_10  r_n_13
+"""
+
+# Merge the similar cells
+wb= Workbook()
+ws=wb.active
 
 rowInd=1
 colInd=1
